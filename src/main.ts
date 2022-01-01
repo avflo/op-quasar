28df12a5a9790e23bab8d7ab65ac5d9ed9847ec4
avflo <bastian.avflo@gmail.com>
2022-01-01 19:05:27 -0300

    :lock: add global exception filter
    add global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { AllExceptionsFilter } from './exception.filter';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
+  // 🐱 NESTJS
   const app = await NestFactory.create(AppModule);
+  // HANDLE EXCEPTIONS
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  // CONFIG
   const config = app.get<ConfigService>(ConfigService);
   app.use(helmet());
 
+  // ⚡ INIT SERVER
   await app.listen(config.get('port'));
+
+  // EXIT EVENT LISTENER
+  process.on('SIGINT', function () {
+    console.log('Gracefully shutting down from SIGINT (Ctrl-C)');
+    // some other closing procedures go here
+    process.exit(1);
+  });
 }
 bootstrap();
