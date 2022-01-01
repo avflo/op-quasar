ec79a0148f370f7e86538e3135fb4dbbc46cba8e
avflo <bastian.avflo@gmail.com>
2022-01-01 18:50:11 -0300

    :package: add helmet
    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService>(ConfigService);
+  app.use(helmet());
+
   await app.listen(config.get('port'));
 }
 bootstrap();
